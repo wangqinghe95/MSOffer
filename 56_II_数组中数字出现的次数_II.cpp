fb0d7c9d55ec0edc1bfcc1461ee805c7b29d8871
wanghao <675072584@qq.com>
2021-09-09 11:09:13 +0800

    [add] 位运算

---
+++ 56_II_数组中数字出现的次数_II.cpp
@@ -0,0 +1,27 @@
+
+/*
+位运算：
+*/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // vector<int> count(32,0);
+        int count[32] = {0};
+
+        for (auto num : nums) {
+            for (int j = 0; j < 32; ++j) {
+                count[j] += num & 1;
+                num >>= 1;
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < 32; ++i) {
+            res <<= 1;
+            res |= count[31-i] % 3;
+        }
+
+        return res;
+    }
+};
