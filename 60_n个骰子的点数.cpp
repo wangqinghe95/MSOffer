f33bac947ad993117d50852080a3ad6cdd45381b
wanghao <675072584@qq.com>
2021-09-15 17:58:52 +0800

    [add] dp解法

---
+++ 60_n个骰子的点数.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<double> dicesProbability(int n) {
+        // vector<double> res(6,1/6);
+        vector<double> dp(6, 1.0/6.0);
+        for (int i = 2; i <= n; ++i) {
+            vector<double> tmp(5*i+1, 0);
+            for (int j = 0; j < dp.size(); ++j) {
+                for (int k = 0; k < 6; k++) {
+                    tmp[j+k] += dp[j] / 6.0;
+                }
+            }
+            dp = tmp;
+        }
+        return dp;
+    }
+};
+
+int main() {
+    Solution a;
+    vector<double> res = a.dicesProbability(2);
+    // double b = (double)1 / 6;
+    return 0;
+}
