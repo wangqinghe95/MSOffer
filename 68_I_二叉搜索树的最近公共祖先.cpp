29a556b2b156bdccfe4b5761116df24d6108d711
wanghao <675072584@qq.com>
2021-09-25 12:02:02 +0800

    [add] 二叉搜索树的公共结点

---
+++ 68_I_二叉搜索树的最近公共祖先.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+/*
+二叉搜索树的公共节点：可以利用二叉搜索树的特性，左子树的值都小于根节点，右子树的值都不小于根节点
+*/
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* ancestor = root;
+        while (true) {
+            if (p->val < ancestor->val && q->val < ancestor->val) {
+                ancestor = ancestor->left;
+            }
+            else if (p->val > ancestor->val && q->val > ancestor->val) {
+                ancestor = ancestor->right;
+            }
+            else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+};
