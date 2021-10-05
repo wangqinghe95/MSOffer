edef7c543a22d53145507457008b5b88749db391
wanghao <675072584@qq.com>
2021-10-05 16:06:13 +0800

    [add] II-2

--- II-2_二进制加法.cpp
+++ II-2_二进制加法.cpp
@@ -2,7 +2,35 @@
 #include<algorithm>
 using namespace std;
 
+// method of subduction to simulate division
 class Solution {
+public:
+    string addBinary(string a, string b) {
+        int len1 = a.size() - 1;
+        int len2 = b.size() - 1;
+
+        string res;
+        int carry = 0;
+        while (len1 >= 0 || len2 >= 0) {
+            int num1 = len1 >= 0 ? a[len1] - '0' : 0;
+            int num2 = len2 >= 0 ? b[len2] - '0' : 0;
+            int sum = num2 + num1 + carry;
+            carry = sum / 2;
+            res.push_back((sum % 2 + '0')); 
+            len1--;
+            len2--;
+        }
+
+        if (carry != 0) {
+            res.push_back(carry  + '0');
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+class Solution_1 {
 public:
     string addBinary(string a, string b) {
         int lenA = a.length()-1;
