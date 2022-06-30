ade6299fa11f866937ee86fcc66b1781d93f087c
王清河 <48783271+wangqinghe95@users.noreply.github.com>
2022-06-30 14:10:46 +0800

    [add] merge more ranges

---
+++ II_74_ 合并区间.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.size() == 0){
+            return {};
+        }
+
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> merged;
+        for (int i = 0; i < intervals.size(); ++i){
+            int L = intervals[i][0];
+            int R = intervals[i][1];
+            if (!merged.size() || merged.back()[1] < L) {
+                merged.push_back({L,R});
+            }
+            else {
+                merged.back()[1] = max(merged.back()[1], R);
+            }
+        }
+        return merged;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> internals{{1,3},{2,6},{8,10},{15,18}};
+    Solution a;
+    a.merge(internals);
+    return 0;
+}
