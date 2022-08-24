9289ea6843e058d4c430a968ac595c8ad1ff35f4
王清河 <48783271+wangqinghe95@users.noreply.github.com>
2022-08-24 17:13:31 +0800

    [add]

---
+++ II_86_分割回文子字符串.cpp
@@ -0,0 +1,37 @@
+
+
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        n = s.size();
+
+        hash.assign(n, vector<int>(n, 1));
+        for (int i = n-1; i >= 0; --i) {
+            for (int j = i+1; j < n; ++j) {
+                hash[i][j] = (s[i] == s[j] && hash[i+1][j-1]);
+            }
+        }
+        dfs(s, 0);
+        return res;
+    }
+private:
+    void dfs(const string& s, int index) {
+        if (index == n) {
+            res.push_back(ans);
+            return;
+        }
+
+        for (int i = index; i < n; ++i) {
+            if(hash[index][i]) {
+                ans.push_back(s.substr(index, i-index+1));
+                dfs(s, i+1);
+                ans.pop_back();
+            }
+        }
+    }
+private:
+    vector<vector<int>> hash;
+    vector<vector<string>> res;
+    vector<string> ans;
+    int n;
+};
